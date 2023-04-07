2ad971cafabd84cb615e6f25671ffc7852211030
MarianneKaiu <lapierre.marianne@gmail.com>
2023-04-07 16:50:10 +0200

    btn-realisation

--- src/components/GridImage.js
+++ src/components/GridImage.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 
 const GridImage = ({ image }) => {
     const [toggleLoupe, setToggleLoupe] = useState(false);
-    // Objectif : réussir à ajouter dynamiquement une class "vertical" et une class "horizontal"
+    const [isLiked, setIsLiked] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(false);
+
     useEffect(() => {
         console.log(image);
     });
@@ -10,19 +12,45 @@ const GridImage = ({ image }) => {
     return (
         <div className="image">
             <img src={`${image.chemin}.jpg`} alt={image.name} />
-            <button
-                className="loupe-container"
-                onClick={() => {
-                    toggleLoupe ? setToggleLoupe(false) : setToggleLoupe(true);
-                }}
-            >
-                <i class="fa-solid fa-magnifying-glass loupe"></i>
-            </button>
+            <div className="button-container">
+                <button
+                    className="loupe-container"
+                    onClick={() => {
+                        toggleLoupe
+                            ? setToggleLoupe(false)
+                            : setToggleLoupe(true);
+                    }}
+                >
+                    <i class="fa-solid fa-magnifying-glass loupe"></i>
+                </button>
+                <button
+                    className="heart-container"
+                    onClick={() => {
+                        isLiked ? setIsLiked(false) : setIsLiked(true);
+                    }}
+                >
+                    {isLiked ? (
+                        <i class="fa-solid fa-heart heart"></i>
+                    ) : (
+                        <i class="fa-regular fa-heart heart"></i>
+                    )}
+                </button>
+                <button className="trash-container">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+                <button className="updateBtn-container">
+                    <i class="fa-regular fa-pen-to-square"></i>
+                </button>
+            </div>
+
             <div
                 id="image-content-container"
                 className={toggleLoupe ? "active" : ""}
             >
-                <h2>{image.name}</h2>
+                <div className="image-content-header">
+                    <h2>{image.name}</h2>
+                </div>
+
                 <p>{image.description}</p>
             </div>
         </div>
--- src/components/TextTyping.js
+++ src/components/TextTyping.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 // On démarre les compteurs à 0
 
